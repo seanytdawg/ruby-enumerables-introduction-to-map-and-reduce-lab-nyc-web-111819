2d167d734e09b70bfd1e124875a1e464b7048c37
Sean Tarzy <seantarzy@gmail.com>
2019-11-15 05:05:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,7 @@ end
 def reduce_to_any_true(source_array)
 total = 0 
   while total < source_array.length do 
-  return true if !source_array[total]
+  return true if source_array[total]
     total += 1 
   end 
   return false 
